7ffb6d50dd89b329b658f2edcb1163b28309e43e
teoya <mattelacorte@gmail.com>
2022-10-02 21:35:51 +0200

    commit

---
+++ js/page-transition.js
@@ -0,0 +1,22 @@
+
+    const transitionElement = document.querySelector('.transition-element');
+    const anchors = document.querySelectorAll('a:not([target="_blank"]');
+
+    setTimeout(() => {
+        transitionElement.classList.remove('is-active');
+    }, 300);
+
+    for (let i = 0; i < anchors.length; i++) {
+        const anchor = anchors[i];
+
+        anchor.addEventListener('click', e => {
+            e.preventDefault();
+            let target = e.target.href;
+
+            transitionElement.classList.add('is-active');
+
+            setTimeout(function(){
+                location.href= target;
+            } , 1000);
+        })
+    }
