2fd1ff89276dec8507b6b68610c3c5a7a41f2ff0
teoya <mattelacorte@gmail.com>
2022-10-03 21:58:25 +0200

    commit

--- js/app.js
+++ js/app.js
@@ -1,5 +1,34 @@
 gsap.registerPlugin(ScrollTrigger);
 
+
+gsap.from(".apto-logo", {
+    y: "-100vh",
+    opacity: 0,
+    width: "200em",
+    duration: 1, 
+    ease: "none",
+    scrollTrigger: {
+        trigger: ".apto-logo", 
+        start: "30% 30%",
+        end: "1000vh 20%",
+        scrub: true,
+        toggleAction: "restart none none none",
+    }
+})
+
+gsap.from(".payoff", {
+    opacity: 0,
+    duration: 1, 
+    scrollTrigger: {
+        trigger: ".payoff", 
+        start: "-900vh 70%",
+        end: "-700vh 70%",
+        scrub: true,
+        toggleAction: "restart none none none",
+    }
+})
+
+
 gsap.to(".feature", {
     duration: 1, 
     scrollTrigger: {
