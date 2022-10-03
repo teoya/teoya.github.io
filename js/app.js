b7a73fe2c8e9d4c8c09fe622f45f5e0ccc270495
teoya <mattelacorte@gmail.com>
2022-10-03 17:41:10 +0200

    commit

--- js/app.js
+++ js/app.js
@@ -2,52 +2,37 @@ gsap.registerPlugin(ScrollTrigger);
 
 let mm = gsap.matchMedia();
 
-mm.add("(min-width: 768px)", () => {
-
-    gsap.to(".hero-text", {
-        y: 300,
-        fontSize: "16rem",
-        opacity: 1,
-        duration: 4, 
-        scrollTrigger: {
-            trigger: ".apto-hero", 
-            start: "50% 80%",
-            end: "50% 30%",
-            scrub: 0.3,
-            toggleAction: "restart paused  none none",
-            //markers: true,
-        }
-    })
-
-})
-
 gsap.to(".hero-text", {
-    y: 1000,
-    fontSize: "16rem",
+    y: "120vh",
+    fontSize: "8rem",
     opacity: 1,
-    duration: 4, 
+    duration: 1, 
+    ease: "none",
     scrollTrigger: {
         trigger: ".apto-hero", 
         start: "50% 80%",
         end: "50% 30%",
-        scrub: 0.3,
-        toggleAction: "restart paused  none none",
-        //markers: true,
-    }
-})
-
-/*
-gsap.to(".square", {
-    x: 800,
-    duration: 4,
-    scrollTrigger: {
-        trigger: ".square", 
-        start: "top 80%",
-        end: "top 30%",
         scrub: true,
         toggleAction: "restart none none none",
         markers: true,
-
-        //toggleClass: "bella"
     }
-})*/
+})
+
+mm.add("(min-width: 768px)", () => {
+
+    gsap.to(".hero-text", {
+        y: "120vh",
+        fontSize: "16rem",
+        opacity: 1,
+        duration: 1, 
+        scrollTrigger: {
+            trigger: ".apto-hero", 
+            start: "50% 80%",
+            end: "50% 30%",
+            scrub: true,
+            toggleAction: "restart none none none",
+            markers: true,
+        }
+    })
+
+})
