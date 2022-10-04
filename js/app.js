8124c9f62f6b1e80dfac09183bd8664fa9eaa0b7
teoya <mattelacorte@gmail.com>
2022-10-04 11:43:23 +0200

    commit

--- js/app.js
+++ js/app.js
@@ -1,11 +1,30 @@
 gsap.registerPlugin(ScrollTrigger);
+/*
+let backtl = gsap.timeline({
+    duration: 1,
+    scrollTrigger: {
+        trigger: ".apto-description", 
+        start: "85% 100%",
+        end: "85% 90%",
+        scrub: 1,
+        toggleAction: "restart none none none",
+        markers: true
+    }
+})
 
+backtl.to(".wrapper", {
+    background: "#E53B1A20",
+}).to(".wrapper", {
+    background: "#E53B1A00",
+})
 
+*/
 gsap.from(".apto-logo", {
     y: "-100vh",
     opacity: 0,
     width: "200em",
     duration: 1, 
+    filter: "blur(4em)",
     ease: "none",
     scrollTrigger: {
         trigger: ".apto-logo", 
@@ -18,6 +37,7 @@ gsap.from(".apto-logo", {
 
 gsap.from(".payoff", {
     opacity: 0,
+    filter: "blur(1em)",
     duration: 1, 
     scrollTrigger: {
         trigger: ".payoff", 
@@ -28,9 +48,24 @@ gsap.from(".payoff", {
     }
 })
 
-
-gsap.to(".feature", {
+gsap.from(".sub-payoff", {
+    opacity: 0,
+    filter: "blur(1em)",
     duration: 1, 
+    scrollTrigger: {
+        trigger: ".payoff", 
+        start: "-900vh 50%",
+        end: "-850vh 50%",
+        scrub: 1,
+        toggleAction: "restart none none none",
+        //markers: true
+    }
+})
+
+
+gsap.from(".feature", {
+    duration: 1, 
+    background: "radial-gradient(circle at 50% 300%, black 0%, #ffffff00 100%);",
     scrollTrigger: {
         trigger: ".feature", 
         start: "50% 50%",
@@ -38,8 +73,25 @@ gsap.to(".feature", {
         pin: true,
         scrub: true,
         toggleAction: "restart none none none",
+        //markers: true
     }
+    
+});
+gsap.to(".feature", {
+    duration: 1, 
+    background: "radial-gradient(circle at 50% -300%, black 0%, #ffffff00 100%);",
+    scrollTrigger: {
+        trigger: ".feature", 
+        start: "50% 50%",
+        end: "50% -100%",
+        pin: true,
+        scrub: true,
+        toggleAction: "restart none none none",
+        //markers: true
+    }
+    
 })
+
 
 /*
 let mm = gsap.matchMedia();
